1714e1b38825e0f88327d67de0e08575dc9c0438
Bensadik Nada <nadabensadik@gmail.com>
2025-02-13 10:07:56 +0100

    fixing bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Home from './Pages/Home.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename='/federation-royale'>
       <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
